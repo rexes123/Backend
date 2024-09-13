33498e817da24d194f2e4fe3e598f76f9fce4cc1
chinhong <chinhong@techdevs.com>
2024-09-13 10:48:54 +0800

    update

--- api/index.js
+++ api/index.js
@@ -89,14 +89,14 @@ app.post('/trips', async function addTrip(req, res) {
   const client = await pool.connect();
   try {
     const { name, type, purpose, flight, depart_from, destination, budget_limit,
-      start_date, end_date, check_in, check_out, hotel, status } = req.body;
+      start_date, end_date, check_in, check_out, hotel } = req.body;
 
     // Validate the request
     const param = [name, type, purpose, flight, depart_from, destination, budget_limit,
-      start_date, end_date, check_in, check_out, hotel, status];
+      start_date, end_date, check_in, check_out, hotel];
 
 
-    const query = 'INSERT INTO trips(name, type, purpose, flight, depart_from, destination, budget_limit, start_date, end_date, check_in, check_out, hotel, status) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *';
+    const query = 'INSERT INTO trips(name, type, purpose, flight, depart_from, destination, budget_limit, start_date, end_date, check_in, check_out, hotel) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 RETURNING *';
     const result = await client.query(query, param);
 
     res.status(201).json({
